05efd8df55e620f81af018f0acad188ef6447d1d
Reski Rukmantiyo <reski.rukmantio@lintasarta.co.id>
2024-11-07 05:49:08 +0700

    feat: added cronjob

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"time"
+
 	"github.com/reski-rukmantiyo/download-exporter/pkg/download"
 	"gofr.dev/pkg/gofr"
 	"gofr.dev/pkg/gofr/http/response"
@@ -20,6 +23,15 @@ func main() {
 	app.GET("/", func(ctx *gofr.Context) (interface{}, error) {
 
 		return "Hello Download Exporter!", nil
+	})
+
+	// Run the cron job every 1 hour
+	app.AddCronJob("* * * * *", "", func(ctx *gofr.Context) {
+		log.Printf("Cron job running at %s", time.Now().String())
+
+		download.Download()
+
+		log.Printf("Cron job finished at %s", time.Now().String())
 	})
 
 	// Runs the server, it will listen on the default port 8000.
